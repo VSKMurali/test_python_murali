a18cac80cb397bcba5002ccd3998d3bdff0a927e
VSKMurali <murali85career@outlook.com>
2023-07-23 16:18:48 +0530

    Getting input from user

---
+++ samples/UserInput.py
@@ -0,0 +1,13 @@
+print("Enter value of A")
+a = int(input())
+print("Enter value of B")
+b = int(input())
+print("Enter value of C")
+c = int(input())
+
+sumMultiply = a * b * c
+sumAddition = a + b + c
+
+print("sumMultiply:",sumMultiply)
+print("sumAddition:",sumAddition)
+print("result ",sumMultiply/sumAddition)
