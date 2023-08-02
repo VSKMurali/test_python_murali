f3136149e152cf146c7826587de2713546701a63
VSKMurali <murali85career@outlook.com>
2023-08-02 21:29:11 +0530

    Function class added

---
+++ samples/FunctionExample1.py
@@ -0,0 +1,11 @@
+print("Enter value of A")
+a = int(input())
+print("Enter value of B")
+b = int(input())
+
+# function with two arguments1
+def add_numbers(a, b):
+    sum = a + b
+    print('Sum: ', sum)
+
+add_numbers(a,b)
