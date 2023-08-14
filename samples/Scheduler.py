dcb454c9883363934f3dd65700cbdfd5d9b2bde0
VSKMurali <murali85career@outlook.com>
2023-08-14 21:32:01 +0530

    This commit will add scheduler functionality

---
+++ samples/Scheduler.py
@@ -0,0 +1,45 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from random import *
+import json
+import os.path
+
+def some_job():
+    print("Decorated job")
+    # Pick a user count from 0 to 1000
+    users_count = randint(0, 1000)
+
+    # Pick a mode from the below list
+    mode_list = ['VOD', 'Live']
+    mode = choice(mode_list)
+
+    # Pick a platform from the below list
+    platform_list = ['TV', 'Mobile', 'Computer']
+    platform = choice(platform_list)
+
+    # Create a JSON Object from the random output
+    data = {}
+    data['platform'] = platform
+    data['mode'] = mode
+    data['users'] = users_count
+    json_data = json.dumps(data)
+
+    # Print the JSON object in console
+    print(json_data)
+
+    # Save the JSON object in the log file
+    directory = 'D:/elk/logs_folder/'
+    file = os.path.join(directory, 'logs1.json')
+    print('file name ', file)
+
+    save_file = open(file, "w")
+    # json.dump(json_data, save_file)
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    save_file.close()
+
+    print('End')
+
+# Scheduling every 5 minutes
+scheduler = BlockingScheduler()
+scheduler.add_job(some_job, 'interval', minutes=5)
+scheduler.start()
